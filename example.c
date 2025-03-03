adb4cb8b17cefcaf7ef22cc8cd90afc32603ee3f
Brian Downs <brian.downs@gmail.com>
2025-03-03 09:43:31 -0700

    updates
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- example.c
+++ example.c
@@ -15,6 +15,8 @@ main(int argc, char **argv)
     s_log(S_LOG_INFO, 
         s_log_string("msg", "record added successfully"), 
         s_log_string("name", "Brian"), 
+        s_log_float("pi", 3.141),
         s_log_double("elapsed", 5.76));
+    s_log(S_LOG_DEBUG, s_log_string("XXX", "here"));
     s_log(S_LOG_FATAL, s_log_string("msg", "dead :("));   
 }
